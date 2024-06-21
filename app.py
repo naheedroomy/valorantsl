9e248a975fd8b70156f799d5e1bcc52f46b259b5
Naheed Roomy <46345736+naheedroomy@users.noreply.github.com>
2024-06-21 18:06:24 +0200

    init
    init

---
+++ app.py
@@ -0,0 +1,56 @@
+import os
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from starlette.middleware.sessions import SessionMiddleware
+from fastapi.responses import JSONResponse
+from fastapi_discord import RateLimited, Unauthorized
+from fastapi_discord.exceptions import ClientSessionNotInitialized
+
+from routes.discord import discord_router
+from routes.valorant import valorant
+
+app = FastAPI()
+
+# Configure allowed origins for CORS
+allowed_origins = [
+    "http://localhost",  # For local development
+    "http://127.0.0.1",  # For local development
+    # Add your server's IP address or domain name here for production
+]
+
+# Add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=allowed_origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Add Session middleware
+app.add_middleware(
+    SessionMiddleware,
+    secret_key=os.getenv("SESSION_SECRET_KEY"),
+)
+
+# Include routers
+app.include_router(discord_router)
+app.include_router(valorant)
+
+
+# Exception Handlers
+@app.exception_handler(Unauthorized)
+async def unauthorized_error_handler(_, __):
+    return JSONResponse({"error": "Unauthorized"}, status_code=401)
+
+@app.exception_handler(RateLimited)
+async def rate_limit_error_handler(_, e: RateLimited):
+    return JSONResponse(
+        {"error": "RateLimited", "retry": e.retry_after, "message": e.message},
+        status_code=429,
+    )
+
+@app.exception_handler(ClientSessionNotInitialized)
+async def client_session_error_handler(_, e: ClientSessionNotInitialized):
+    print(e)
+    return JSONResponse({"error": "Internal Error"}, status_code=500)
