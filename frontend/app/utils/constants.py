ab63f0c2ce615129068f90856275d129d7af8552
Naheed Roomy <nroomy1@gmail.com>
2025-04-22 16:12:14 +0530

    change: folder restructure

---
+++ frontend/app/utils/constants.py
@@ -0,0 +1,16 @@
+# Constants for the application
+import os
+
+RANK_MAPPING = {
+    'Iron 1': 3, 'Iron 2': 4, 'Iron 3': 5,
+    'Bronze 1': 6, 'Bronze 2': 7, 'Bronze 3': 8,
+    'Silver 1': 9, 'Silver 2': 10, 'Silver 3': 11,
+    'Gold 1': 12, 'Gold 2': 13, 'Gold 3': 14,
+    'Platinum 1': 15, 'Platinum 2': 16, 'Platinum 3': 17,
+    'Diamond 1': 18, 'Diamond 2': 19, 'Diamond 3': 20,
+    'Ascendant 1': 21, 'Ascendant 2': 22, 'Ascendant 3': 23,
+    'Immortal 1': 24, 'Immortal 2': 25, 'Immortal 3': 26,
+    'Radiant': 27
+}
+
+DISCORD_SERVER_INVITE = os.getenv("DISCORD_SERVER_INVITE")
